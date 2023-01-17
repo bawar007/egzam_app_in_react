86a37c3c65f4cce1608236a354b7e644e208b251
bawar007 <barski001@o2.pl>
2023-01-17 23:15:07 +0100

    home page

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import { NavLink } from "react-router-dom";
+
+import "../../styles/HomePage.css";
+
 const HomePage = () => {
-  return <h3>Home Page</h3>;
+  return (
+    <div className="homePage">
+      <h2>Witaj na naszej stronie</h2>
+      <div className="img">
+        <img src="/picture/img1.jpg" alt="" srcset="" />
+        <img src="/picture/img2.jpg" alt="" srcset="" />
+        <img src="/picture/img3.jpg" alt="" srcset="" />
+      </div>
+      <p>
+        Tutaj nauczyć się wielu ciekawych rzeczy, które pomogą Ci zdać przyszłe
+        egzaminy.
+      </p>
+      <div className="my_buttons">
+        <NavLink to="/pass" state="egzamin">
+          rozpocznij Egzamin
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
