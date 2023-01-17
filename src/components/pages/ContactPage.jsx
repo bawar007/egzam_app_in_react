3dfb2899afa612ba3e7a663fe537c56b1dd31854
bawar007 <barski001@o2.pl>
2023-01-17 22:42:33 +0100

    add contact page

--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -1,10 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
+
+import "../../styles/ContactPage.css";
 
 const ContactPage = () => {
+  const [state, setValue] = useState({
+    value: "",
+    isEmpty: true,
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setValue((prevState) => {
+      return { ...prevState, value: "" };
+    });
+  };
+
+  const handleChange = (e) => {
+    setValue(() => {
+      return {
+        value: e.target.value,
+        isEmpty: false,
+      };
+    });
+  };
   return (
-    <div>
-      <h3>ContactPage</h3>
-      <h3>ContactPage</h3>
+    <div className="contact">
+      <form onSubmit={handleSubmit}>
+        <h3>Napisz do nas</h3>
+        <textarea
+          value={state.value}
+          onChange={handleChange}
+          placeholder="Napisz wiadomość..."
+        ></textarea>
+        <button>Wyślij</button>
+      </form>
     </div>
   );
 };
